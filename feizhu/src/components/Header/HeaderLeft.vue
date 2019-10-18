ece485ee03fc765ae4652e7cb9a1f19bca446373
sihuanian <2267322300@qq.com>
2019-10-18 22:42:40 +0800

    头部左侧

--- feizhu/src/components/Header/HeaderLeft.vue
+++ feizhu/src/components/Header/HeaderLeft.vue
@@ -3,23 +3,21 @@
     <ul id="J_TSiteNavBdL" class="tsite-nav-bd-l">
       <li class="menu home" data-spm>
         <div class="menu-hd">
-          <a href="//www.fliggy.com/" target="_top">
+          <router-link to="/">
             <span class="iconfont icon-fangzi"></span>
             <span>飞猪</span>
-          </a>
+          </router-link>
         </div>
       </li>
       <li id="J_TLoginInfo" class="menu" data-spm>
         <div id="J_TLoginInfoHd" class="menu-hd">
           <span class="l">Hi，最近想去哪里玩？</span>
-          <a
-            href="https://login.taobao.com/member/login.jhtml?f=top&amp;redirectURL=https%3A%2F%2Fwww.fliggy.com%2F%3Fttid%3Dsem.000000736%26hlreferid%3Dbaidu.082076%26route_source%3Dseo"
-            target="_top"
-          >请登录</a>
-          <a
-            href="http://reg.taobao.com/member/new_register.jhtml?from=tbtop&amp;ex_info=&amp;ex_sign="
-            target="_top"
-          >免费注册</a>
+          <router-link to="/user" v-if="username === 'sihuanian'">{{username}}</router-link>
+          <div v-else style="display: inline-block;">
+            <router-link to="/login">请登录</router-link>
+            <router-link to="/regist">免费注册</router-link>
+          </div>
+          <span v-if="username === 'sihuanian'" to="/" @click="esc">退出</span>
         </div>
       </li>
     </ul>
@@ -27,7 +25,24 @@
 </template>
 
 <script>
-export default {}
+import { mapGetters } from 'vuex'
+export default {
+  created () {
+    console.log(this.username, this.password)
+  },
+  computed: {
+    ...mapGetters('user', {
+      username: 'getUsername',
+      avatar: 'getUserAvatar'
+    })
+  },
+  methods: {
+    esc () {
+      console.log('esc')
+      this.$store.dispatch('user/esc')
+    }
+  }
+}
 </script>
 
 <style scoped lang="stylus">
@@ -50,11 +65,12 @@ export default {}
         margin-right 7px
         font-size 12px
         color #6C6C6C
+        text-decoration none
+        &:hover
+          color #f39b03
         .icon-fangzi
           margin-right 5px
           line-height 24px
-          line-height 20px\0
-          _line-height 24px
           color #ffc300!important
         span
           float left
