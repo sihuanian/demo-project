ea6fb9191a772c9a4dae19d36754228019dea942
sihuanian <2267322300@qq.com>
2019-10-18 22:52:03 +0800

    引入vuex

--- feizhu/src/main.js
+++ feizhu/src/main.js
@@ -3,19 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store/index'
 import VueLazyLoad from 'vue-lazyload'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import 'normalize.css'
 
-Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.use(VueLazyLoad)
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
