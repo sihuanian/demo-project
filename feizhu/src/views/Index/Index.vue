2c7bb9152f9a7e63829db455c1cac633580f2d5f
sihuanian <2267322300@qq.com>
2019-10-27 23:05:40 +0800

    首页

---
+++ feizhu/src/views/Index/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <my-header />
+    <my-input />
+    <my-navBar />
+    <my-search />
+  </div>
+</template>
+
+<script>
+import MyHeader from '@/components/Header/Header.vue'
+import MyInput from '@/components/Input/Input.vue'
+import MyNavBar from '@/components/NavBar/NavBar.vue'
+import MySearch from '@/components/Search/Search.vue'
+export default {
+  name: 'App',
+  components: {
+    MyHeader,
+    MyInput,
+    MyNavBar,
+    MySearch
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
