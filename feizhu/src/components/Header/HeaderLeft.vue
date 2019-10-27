50d3d3f2552f812376fe01bea945ad62a4c1edca
sihuanian <2267322300@qq.com>
2019-10-27 23:12:01 +0800

    头部左侧部分

--- feizhu/src/components/Header/HeaderLeft.vue
+++ feizhu/src/components/Header/HeaderLeft.vue
@@ -1,35 +1,30 @@
 <template>
-  <div>
-    <ul id="J_TSiteNavBdL" class="tsite-nav-bd-l">
-      <li class="menu home" data-spm>
-        <div class="menu-hd">
-          <router-link to="/">
-            <span class="iconfont icon-fangzi"></span>
-            <span>飞猪</span>
-          </router-link>
+  <ul id="J_TSiteNavBdL" class="tsite-nav-bd-l">
+    <li class="menu home" data-spm>
+      <div class="menu-hd">
+        <router-link to="/">
+          <span class="iconfont icon-fangzi"></span>
+          <span>飞猪</span>
+        </router-link>
+      </div>
+    </li>
+    <li id="J_TLoginInfo" class="menu" data-spm>
+      <div id="J_TLoginInfoHd" class="menu-hd">
+        <span class="l">Hi，最近想去哪里玩？</span>
+        <router-link to="/user" v-if="username === 'sihuanian'">{{username}}</router-link>
+        <div v-else style="display: inline-block;">
+          <router-link to="/login">请登录</router-link>
+          <router-link to="/regist">免费注册</router-link>
         </div>
-      </li>
-      <li id="J_TLoginInfo" class="menu" data-spm>
-        <div id="J_TLoginInfoHd" class="menu-hd">
-          <span class="l">Hi，最近想去哪里玩？</span>
-          <router-link to="/user" v-if="username === 'sihuanian'">{{username}}</router-link>
-          <div v-else style="display: inline-block;">
-            <router-link to="/login">请登录</router-link>
-            <router-link to="/regist">免费注册</router-link>
-          </div>
-          <span v-if="username === 'sihuanian'" to="/" @click="esc">退出</span>
-        </div>
-      </li>
-    </ul>
-  </div>
+        <span v-if="username === 'sihuanian'" to="/" @click="esc">退出</span>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 export default {
-  created () {
-    console.log(this.username, this.password)
-  },
   computed: {
     ...mapGetters('user', {
       username: 'getUsername',
@@ -47,11 +42,12 @@ export default {
 
 <style scoped lang="stylus">
 .tsite-nav-bd-l
-  font-size 12px
   float left
+  margin 0
   .menu
     float left
     .menu-hd
+      font-size 12px
       z-index 10002
       position relative
       padding 0 6px
