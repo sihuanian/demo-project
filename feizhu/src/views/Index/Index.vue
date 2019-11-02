18f0245fb3752e102b3e4ab76dfb022ea8c29c10
sihuanian <2267322300@qq.com>
2019-11-02 23:29:42 +0800

    发送请求与组件通信

--- feizhu/src/views/Index/Index.vue
+++ feizhu/src/views/Index/Index.vue
@@ -4,6 +4,7 @@
     <my-input />
     <my-navBar />
     <my-search />
+    <my-route :fights="airplanes" />
   </div>
 </template>
 
@@ -12,16 +13,68 @@ import MyHeader from '@/components/Header/Header.vue'
 import MyInput from '@/components/Input/Input.vue'
 import MyNavBar from '@/components/NavBar/NavBar.vue'
 import MySearch from '@/components/Search/Search.vue'
+import MyRoute from '@/components/AirRoute/AirRoute.vue'
+// import axios from '@/api/fetch.js'
 export default {
   name: 'App',
   components: {
     MyHeader,
     MyInput,
     MyNavBar,
-    MySearch
+    MySearch,
+    MyRoute
+  },
+  data () {
+    return {
+      airplanes: null
+    }
+  },
+  created () {
+    this.$jsonp('/api/rule/promotion', {
+      ruleId: 1971,
+      cityName: '北京',
+      queryCount: 10,
+      _input_charset: 'utf-8',
+      callback: 'jsonp_1572621823386_95823'
+    }).then(json => {
+      // Success.
+      this.airplanes = json.result
+    }).catch(err => {
+      // Failed.
+      console.log(err)
+    })
+  },
+  beforeMount () {
+  },
+  provide () {
+    return {
+      fights: this.airplanes
+    }
+  },
+  methods: {
+    // /api/rule/promotion?ruleId=1971&cityName=北京&queryCount=10&_input_charset=utf-8&callback=jsonp_1572621823386_95823
+    getFights () {
+      /* axios
+        .get('/api/rule/promotion?ruleId=1971&cityName=%E5%8C%97%E4%BA%AC&queryCount=10&_input_charset=utf-8&callback=jsonp_1572686264440_47211')
+        .then(res => {
+          console.log(res)
+        }) */
+      this.$jsonp('/api/rule/promotion', {
+        ruleId: 1971,
+        cityName: '北京',
+        queryCount: 10,
+        _input_charset: 'utf-8',
+        callback: 'jsonp_1572621823386_95823'
+      }).then(json => {
+        // Success.
+        this.airplanes = json.result
+      }).catch(err => {
+        // Failed.
+        console.log(err)
+      })
+    }
   }
 }
 </script>
 
-<style scoped lang="stylus">
-</style>
+<style scoped lang="stylus"></style>
