9efb11da290ac437ee5b1c654b3ac84ab330de00
sihuanian <2267322300@qq.com>
2019-11-04 16:16:12 +0800

    城市列表

--- feizhu/src/components/Recommend/City.vue
+++ feizhu/src/components/Recommend/City.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="wrapper">
     <ul class="city-container" >
-      <li class="city-item" v-for="(city, index) in citys" :data-city="city" :key="index"
+      <li class="city-item selected" @click="addSelected" :data-city="citys[0]">{{citys[0]}}</li><li class="city-item" v-for="(city, index) in cities" :data-city="city" :key="index"
         @click="addSelected">{{city}}</li>
     </ul>
+    <router-link class="more" tag="div" to='/'>更多国内特价票</router-link>
     <!-- <MyCityList /> -->
     <div class="city-list">
       <MyCityItem v-for="(item, index) in fights" :fightInfo="item" :key="index"
@@ -32,10 +33,22 @@ export default {
     citys: {
       type: Array,
       required: true
+    },
+    ruleId: {
+      type: Number,
+      required: true
     }
     // fights: {
     //   type: Array
     // }
+  },
+  computed: {
+    cities () {
+      const arr = this.citys.slice()
+      arr.shift()
+      // console.log(arr)
+      return arr
+    }
   },
   // inject: ['fights'],
   created () {
@@ -46,9 +59,9 @@ export default {
     // })
   },
   mounted () {
-    this.$nextTick(function () {
-      document.querySelector('.city-item').classList.add('selected')
-    })
+    // this.$nextTick(function () {
+    //   document.querySelector('.city-item').classList.add('selected')
+    // })
   },
   methods: {
     addSelected (e) {
@@ -69,7 +82,7 @@ export default {
     },
     getData (city = '北京', index = 0) {
       this.$jsonp('/api/rule/promotion', {
-        ruleId: 1971 + index,
+        ruleId: this.ruleId + index,
         cityName: city,
         queryCount: 10,
         _input_charset: 'utf-8',
@@ -93,6 +106,7 @@ export default {
   background-color #ffc900
   font-weight 800
 .wrapper
+  position relative
   .city-container
     padding-left 16px
     margin 17px 0
@@ -106,6 +120,16 @@ export default {
       padding: 0 6px;
       color: #3d3d3d;
       font-size 12px
+  .more
+    position absolute
+    right 8px
+    top 3px
+    font-weight: 300;
+    background: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAABNUlEQVR42rVWPUvDUBS9ReogLh1cVLpJt4KCFJRYEeog0qFLoVsdXITSLv6C4qZ/IEuhczfp4tJBCjYgOGbt3CmZhMjxHRBBSpL3hPvgQEjuOfflfrz7JGvhQ/YRyB0WMjUI8SYxwWe+4zfaiOvCu+waAd+IJQbIQUJbcuzEA2kaUkSyIyJys8UX0jP4IuE/IJcaqTtPEXd2Qq21mGeGxR3Rn5wwSWnG3fYRhv0KPl83XP/E/y3FrGoJxiVcXZ7gtnOI1cumi5OE2sJazjNePm+h0zpG67qGcLJt7YTawoaxMY5nRfRvqmhcnGLm79iGaUoHoe2OknkBj/cHOPM8jIZlGweh/LQ/XDB52sN53cPDoJJnG6s7UA+RepLVy1S90VSPCvXDTv+41h84+iNTf+irX1u+AQEoBcaSEpouAAAAAElFTkSuQmCC') no-repeat 100%;
+    background-size: 12px;
+    padding-right: 15px;
+    color #3d3d3d
+    font-size 12px
   .city-list
     &::after
       content ''
