8d98215b1b49c290bb8b6b59073b4c696bef4202
sihuanian <2267322300@qq.com>
2019-11-05 23:42:48 +0800

    mock假数据

--- feizhu/src/mock/mockServer.js
+++ feizhu/src/mock/mockServer.js
@@ -4,8 +4,25 @@
 import Mock from 'mockjs'
 import ZX from './feizhuzhuanxian.json' // webpack在打包时自动解析为js对应的类型(对象)
 import cityList from './cityList.json'
+import forign from './foreign.json'
+import forignCity0 from './foreignCity0.json'
+import forignCity1 from './foreignCity1.json'
+import forignCity2 from './foreignCity2.json'
+import forignCity3 from './foreignCity3.json'
+import forignCity4 from './foreignCity4.json'
+import forignCity5 from './foreignCity5.json'
+import forignCity6 from './foreignCity6.json'
+
 Mock.mock('/zhuanxian', {data: ZX.result})
 Mock.mock('/cityList', {data: cityList})
+Mock.mock('/forign', {data: forign})
+Mock.mock('/foreignCity?id=0', {data: forignCity0})
+Mock.mock('/foreignCity?id=1', {data: forignCity1})
+Mock.mock('/foreignCity?id=2', {data: forignCity2})
+Mock.mock('/foreignCity?id=3', {data: forignCity3})
+Mock.mock('/foreignCity?id=4', {data: forignCity4})
+Mock.mock('/foreignCity?id=5', {data: forignCity5})
+Mock.mock('/foreignCity?id=6', {data: forignCity6})
 
 /*
 // 暴露接口: /goods
