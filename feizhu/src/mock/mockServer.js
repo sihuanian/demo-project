4a107b2933c1b9d587bee0283c65d526c299dde0
sihuanian <2267322300@qq.com>
2019-11-07 10:41:43 +0800

    mock数据

--- feizhu/src/mock/mockServer.js
+++ feizhu/src/mock/mockServer.js
@@ -12,10 +12,20 @@ import forignCity3 from './foreignCity3.json'
 import forignCity4 from './foreignCity4.json'
 import forignCity5 from './foreignCity5.json'
 import forignCity6 from './foreignCity6.json'
+import territory from './territory.json'
+import territoryCity0 from './territoryCity0.json'
+import territoryCity1 from './territoryCity1.json'
+import territoryCity2 from './territoryCity2.json'
+import territoryCity3 from './territoryCity3.json'
+import territoryCity4 from './territoryCity4.json'
+import territoryCity5 from './territoryCity5.json'
+import territoryCity6 from './territoryCity6.json'
+import territoryCity7 from './territoryCity7.json'
+import packageTour0 from './packageTour0.json'
 
 Mock.mock('/zhuanxian', {data: ZX.result})
 Mock.mock('/cityList', {data: cityList})
-Mock.mock('/forign', {data: forign})
+Mock.mock('/foreign', {data: forign})
 Mock.mock('/foreignCity?id=0', {data: forignCity0})
 Mock.mock('/foreignCity?id=1', {data: forignCity1})
 Mock.mock('/foreignCity?id=2', {data: forignCity2})
@@ -23,6 +33,16 @@ Mock.mock('/foreignCity?id=3', {data: forignCity3})
 Mock.mock('/foreignCity?id=4', {data: forignCity4})
 Mock.mock('/foreignCity?id=5', {data: forignCity5})
 Mock.mock('/foreignCity?id=6', {data: forignCity6})
+Mock.mock('/territory', {data: territory})
+Mock.mock('/territoryCity?id=0', {data: territoryCity0})
+Mock.mock('/territoryCity?id=1', {data: territoryCity1})
+Mock.mock('/territoryCity?id=2', {data: territoryCity2})
+Mock.mock('/territoryCity?id=3', {data: territoryCity3})
+Mock.mock('/territoryCity?id=4', {data: territoryCity4})
+Mock.mock('/territoryCity?id=5', {data: territoryCity5})
+Mock.mock('/territoryCity?id=6', {data: territoryCity6})
+Mock.mock('/territoryCity?id=7', {data: territoryCity7})
+Mock.mock('/packageTour?id=0', {data: packageTour0})
 
 /*
 // 暴露接口: /goods
