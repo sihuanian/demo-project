e2b53922b3e02015ed0af70ef76f0eea6533c43e
sihuanian <2267322300@qq.com>
2019-11-17 22:55:06 +0800

    景点推荐

---
+++ feizhu/src/components/RecommendPlace/Recommend.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="field-container" :title="field.title">
+    <router-link to="/">
+      <div class="destination">
+        <span>{{ field.destination }}</span>
+        <span>{{ field.type }}</span>
+      </div>
+      <div class="field-img">
+        <img :src="field.img" alt />
+      </div>
+      <div class="field-info">
+        <div class="field-desc">
+          <img src />
+          {{ field.title }}
+        </div>
+        <div style="padding: 0 12px; box-sizing: border-box; min-height: 23.2px; overflow:hidden;">
+          <div class="field-tag" v-for="(item, index) in field.tags" :key="index" style="display: inline-block;">
+            <img v-if="item.icon" :src="item.icon" alt="">
+            <span v-else class="tag">{{ item.text }}</span>
+          </div>
+        </div>
+        <div class="field-price">
+          <div class="price">
+            <em>￥</em>{{ Math.trunc(field.price) }}<span>起</span>
+          </div>
+          <div class="sale" v-if="field.seller > 0">30天售出{{ field.seller }}件</div>
+        </div>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Recommend',
+  props: {
+    field: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.field-container
+  float left
+  width 227px
+  height 262px
+  margin 0 6px 6px 0
+  position relative
+  transition box-shadow .2s ease-in
+  &:hover
+    box-shadow 0 2px 1px #e1e1e1
+  a
+    display block
+    color #3d3d3d
+    .field-img
+      height 150px
+      overflow hidden
+      background-position 50%
+      img
+        width 100%
+        border 0
+    .destination
+      position absolute
+      background #ffc900
+      left 0
+      top 0
+      font-size 12px
+      span
+        float left
+        padding 2px 6px
+        color #3d3d3d
+    .field-info
+      border 1px solid #f2f3f4
+      border-top-width 0
+      padding-top 12px
+      overflow hidden
+      .field-desc
+        margin 0 12px 5px
+        font-size 14px
+        height 40px
+        overflow hidden
+        img
+          height 16px
+          position relative
+          top 2p
+      .field-tag
+        overflow hidden
+        font-size 12px
+        display inline-block
+        img
+          position relative
+          top 3px
+          height 16px
+          margin-right 2px
+        span
+          padding 0 2px
+          margin 0 2px 0 0
+          display inline-block
+          height 16px
+          border 1px solid rgb(252, 233, 184)
+          color rgb(252, 165, 0)
+      .field-price
+        padding 0 12px 12px
+        overflow hidden
+        .price
+          float left
+          height 18px
+          font-size 16px
+          font-family pingfang sc,Arial
+          line-height 1
+          vertical-align baseline
+          color #ff5741
+          em
+            font-size 12px
+            font-style normal
+          span
+            color #666
+            font-size 12px
+        .sale
+          float right
+          font-size 12px
+          color #a5a5a5
+</style>
---
+++ feizhu/src/components/RecommendPlace/RecommendPlace.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="recommend-wrapper clearfix">
+    <div class="title"><h2>猜你喜欢</h2></div>
+    <div class="recomment-container">
+      <Recommend v-for="(field, index) in fields" :key="index" :field="field" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Recommend from './Recommend'
+import axios from 'axios'
+import '../../mock/mockServer'
+export default {
+  components: {
+    Recommend
+  },
+  data () {
+    return {
+      fields: []
+    }
+  },
+  created () {
+    axios.get('/recommend')
+      .then(res => {
+        console.log(res.data.data)
+        this.fields = res.data.data
+      })
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.recommend-wrapper
+  background-color #fff
+  .title
+    text-align center
+    line-height: 48px;
+    background: #fbfbfb;
+    h2
+      font-size: 18px;
+      line-height 23px
+      color: #000;
+      font-weight: 500;
+      display inline-block
+      background: url(https://gw.alicdn.com/tfs/TB1oLOePXXXXXbnXFXXXXXXXXXX-36-336.png) 0 -143px no-repeat;
+      background-size: 18px auto;
+      padding-left 24px
+  .recomment-container
+    padding 0 12px 6px 13px
+    box-sizing border-box
+    &::after
+      content ''
+      display block
+      height 0
+      clear both
+      visibility hidden
+</style>
