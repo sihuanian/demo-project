6d02cedce46ba8b37c0f9d84ab519cf387d3adfb
sihuanian <2267322300@qq.com>
2019-11-22 23:51:20 +0800

    按需引入element-ui

---
+++ feizhu/src/element/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import { Calendar, ButtonGroup, Button, Row } from 'element-ui'
+
+Vue.component(Calendar.name, Calendar)
+Vue.component(ButtonGroup.name, ButtonGroup)
+Vue.component(Button.name, Button)
+Vue.component(Row.name, Row)
